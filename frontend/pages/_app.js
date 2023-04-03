6db92bb0965ca3790865a30d89c550b6dcca141e
OlivierDAVID <o.david@yahoo.fr>
2023-04-03 16:10:29 +0200

    chains and wallets available in rainbow updated

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,77 +1,86 @@
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 import {
   getDefaultWallets,
+  connectorsForWallets,
   RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { hardhat } from 'wagmi/chains';
+} from "@rainbow-me/rainbowkit";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { hardhat, polygonMumbai, goerli, sepolia } from "wagmi/chains";
 // import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import { publicProvider } from "wagmi/providers/public";
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Montserrat } from 'next/font/google'
-import '@/styles/globals.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Montserrat } from "next/font/google";
+import "@/styles/globals.css";
 
 const { chains, provider } = configureChains(
-  [hardhat],
+  [hardhat, polygonMumbai, goerli, sepolia],
   [
     //alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
+    publicProvider(),
   ]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'Qaptur',
-  chains
-});
+// const { connectors } = getDefaultWallets({
+//   appName: "Qaptur",
+//   chains,
+// });
+
+const connectors = connectorsForWallets([
+  {
+    appName: "Qaptur",
+    groupName: "Recommended",
+    wallets: [metaMaskWallet({ chains })],
+  },
+]);
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
-})
+  provider,
+});
 
 // https://muhimasri.com/blogs/how-to-customize-theme-and-colors-in-material-ui/
 const theme = createTheme({
   status: {
-    danger: '#e53e3e',
+    danger: "#e53e3e",
   },
   palette: {
     primary: {
-      main: '#242330',
+      main: "#242330",
     },
     secondary: {
-      main: '#59CCCF',
+      main: "#59CCCF",
     },
     ternary: {
-      main: '#EF2B5A',
+      main: "#EF2B5A",
     },
     neutral: {
-      main: '#D8D8D8',
+      main: "#D8D8D8",
     },
   },
 });
 
 const montserrat = Montserrat({
-  weight: ['400', '800'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-})
-
+  weight: ["400", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
+        <RainbowKitProvider chains={chains} initialChain={polygonMumbai}>
           <style jsx global>{`
-          html {
-            font-family: ${montserrat.style.fontFamily};
-          }
-        `}</style>
+            html {
+              font-family: ${montserrat.style.fontFamily};
+            }
+          `}</style>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </WagmiConfig>
     </ThemeProvider>
-  )
+  );
 }
